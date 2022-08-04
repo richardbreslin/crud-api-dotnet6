6b2824f66f8b007f53c827d9497ea20cbbf7454c
Richard Breslin <18ribreslin@gmail.com>
2022-08-04 13:56:20 -0400

    Update User.cs

--- Entities/User.cs
+++ Entities/User.cs
@@ -10,7 +10,4 @@ public class User
     public string LastName { get; set; }
     public string Email { get; set; }
     public Role Role { get; set; }
-
-    [JsonIgnore]
-    public string PasswordHash { get; set; }
 }
